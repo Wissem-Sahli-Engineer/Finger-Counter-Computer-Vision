018177bfb8faeec9e5a166f9169992b80edad0fb
Wissem Sahli <contact.ing.wissem@gmail.com>
2026-07-10 18:29:55 +0100

    Base

---
+++ main.py
@@ -0,0 +1,64 @@
+import cv2
+# pyrefly: ignore [missing-import]
+import mediapipe as mp 
+import os , time 
+from Hand_Tracking_Model.utils import handDetector
+from utils import get_fps
+
+
+##################
+""" Arguments """
+##################
+Wcam, Hcam = 1280 , 720 
+
+cap = cv2.VideoCapture(0)
+cap.set(3,Wcam)
+cap.set(4,Hcam)
+
+detector = handDetector(model_path = "Hand_Tracking_Model/hand_landmarker.task",
+                        num_hands = 1,
+                        confidence = 0.6
+                        )
+
+frame_count = 0
+pTime = time.time()
+
+Alert = "Hand is not completly DETECTED ! Try to move it !"
+
+while True:
+
+    # Reading
+    test , img = cap.read()
+    if not test or img is None:
+        break
+
+    # fps and time
+    fps , pTime = get_fps(cap, pTime)
+
+    timestamp_ms = int((frame_count / get_fps(cap,0,type='cap')[0]) * 1000)
+    frame_count += 1
+
+    # flipping the image Y-AXIS : 
+    img = cv2.flip(img,1)
+
+    # preprocessing
+
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    mp_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=img)
+
+    res = detector.landmarker.detect_for_video(mp_img,timestamp_ms) 
+
+    lmList = detector.findHands(img,res, draw =True )
+
+
+    # display 
+    cv2.putText(img,f'FPS : {str(int(fps))}',(40,70), cv2.FONT_HERSHEY_COMPLEX,
+                3, (183,81,93) , 1
+                )
+    cv2.imshow('Live',cv2.cvtColor(img,cv2.COLOR_RGB2BGR))
+
+    if cv2.waitKey(1) & 0xFF == ord(' '):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
